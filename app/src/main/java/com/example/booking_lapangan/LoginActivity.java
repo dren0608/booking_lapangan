ff0fc2fc22487317ae40de27b10c315786174d40
Handyca <hendycayen@gmail.com>
2025-05-28 14:50:25 +0700

    chelsica made new changes

---
+++ app/src/main/java/com/example/booking_lapangan/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.booking_lapangan;
+
+public class LoginActivity {
+}
---
+++ app/src/main/java/com/example/booking_lapangan/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.booking_lapangan;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+
+import com.example.booking_lapangan.R;
+
+public class SplashActivity extends AppCompatActivity {
+3
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000); //waktu tunda 3 detik
+        //button dengan waktu
+        //button biasa
+    }
+}
